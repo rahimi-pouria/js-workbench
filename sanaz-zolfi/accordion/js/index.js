16c8933ed89ba7198543ebcecb82a017271c8b64
Somayeh zolfi <145246539+Szolfi1986@users.noreply.github.com>
2026-05-27 07:47:27 +0330

    Update index.js

--- sanaz-zolfi/accordion/js/index.js
+++ sanaz-zolfi/accordion/js/index.js
@@ -1,14 +1,16 @@
-let header = documet.getElementByClass('header');
+let header = documet.getElementsByClassName('header');
 let headers = Array.From(header);
-let description = document.getElementByClass('description');
+let description = document.getElementsByClassName('description');
 let descriptions Array.From(description);
 
 headers.forEach((item,index), => {
+item.addEventListner('click', () =>{
 des.forEach((des,i), => {
 des.classList.remove('open');
 des.classList.add('close');
 if(index === i){
 des.classList.toggle('close');
 }
+})
 })
 });
